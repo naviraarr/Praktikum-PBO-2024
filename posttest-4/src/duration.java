e62bacce4045ea8ce2e6251d55eb6d2e32e87741
nap <145359806+naviraarr@users.noreply.github.com>
2024-04-01 00:46:40 +0800

    posttest-4

---
+++ posttest-4/src/duration.java
@@ -0,0 +1,22 @@
+class duration extends dvd_film {
+    private int duration;
+
+    public duration(String dvdName, String genre, int releaseYear, String director, int price, int duration) {
+        super(dvdName, genre, releaseYear, director, price);
+        this.duration = duration;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public void tampil(){
+        super.tampil();
+        System.out.println( "  Duration : " + getDuration());
+    }
+}
