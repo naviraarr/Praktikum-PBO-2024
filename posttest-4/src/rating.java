b561c375ac6eb3c9ccf8bb57a1d659b6746af050
nap <145359806+naviraarr@users.noreply.github.com>
2024-04-03 01:55:07 +0800

    posttest-4

---
+++ posttest-4/src/rating.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+class rating extends dvd_film{
+    private double rating;
+
+    public rating(String dvdName, String genre, int releaseYear, String director, int price, double rating){
+        super(dvdName, genre, releaseYear, director, price);
+        if (rating < 0 || rating > 5) {
+            System.out.println("Invalid Rating");
+        } else {
+            this.rating = rating;
+        }
+    }
+
+    public double getRating(){
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        if (rating <0 || rating >5 ){
+            System.out.println("Invalid Rating");
+        }
+        else{
+            this.rating = rating;
+        }
+    }
+
+    //overloading
+    public void tampil(ArrayList<rating> rtg){
+        tampil();
+        System.out.println("  Rating : " + getRating());
+    }
+
+}
